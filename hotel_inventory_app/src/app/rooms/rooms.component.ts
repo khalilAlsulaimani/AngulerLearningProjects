a96ee1f94483e0767c8e20d4c674c17803e93c43
khalil alsulaimani <alsulaimani.2000@outlook.com>
2023-04-16 14:15:38 +0300

    binidng done

---
+++ hotel_inventory_app/src/app/rooms/rooms.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.scss'],
+})
+export class RoomsComponent {
+  hotelName = 'Hilton Hotel';
+  numberOfRooms = 10;
+  hideRooms: boolean = false;
+
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+  }
+}
