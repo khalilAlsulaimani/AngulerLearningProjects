046075c29e16642782dd56b534e637a06851877d
khalil alsulaimani <alsulaimani.2000@outlook.com>
2023-05-15 11:20:14 +0300

    life cycle hooks

--- hotel_inventory_app/src/app/rooms/rooms.component.ts
+++ hotel_inventory_app/src/app/rooms/rooms.component.ts
@@ -11,49 +11,75 @@ export class RoomsComponent {
   numberOfRooms = 10;
   hideRooms: boolean = false;
 
+  selectedRoom!: RoomList;
+
   rooms: Room = {
     totalRooms: 20,
     availableRooms: 10,
     bookedRooms: 5,
   };
 
-  roomList: RoomList[] = [
-    {
-      roomNumber: 1,
-      roomType: 'Deluxe Room',
-      amenity: 'AC, Free Wifi,tv,bathrom,kitchen',
-      price: 500,
-      photos:
-        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nytimes.com%2F2019%2F03%2F20%2Ftravel%2Fshop-hotel-rooms-boutiques-retail.html&psig=AOvVaw3buJ-84TWAHhlYuODHhb0G&ust=1681731559259000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNCx7riorv4CFQAAAAAdAAAAABAE',
-      checkinTime: new Date('11-Nov-2021'),
-      checkoutTime: new Date('21-Nov-2021'),
-      rating: 4.5,
-    },
-    {
-      roomNumber: 2,
-      roomType: 'Deluxe Double Room',
-      amenity: 'AC, Free Wifi,tv,bathrom,kitchen',
-      price: 1000,
-      photos:
-        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nytimes.com%2F2019%2F03%2F20%2Ftravel%2Fshop-hotel-rooms-boutiques-retail.html&psig=AOvVaw3buJ-84TWAHhlYuODHhb0G&ust=1681731559259000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNCx7riorv4CFQAAAAAdAAAAABAE',
-      checkinTime: new Date('10-Nov-2021'),
-      checkoutTime: new Date('20-Nov-2021'),
-      rating: 3.4,
-    },
-    {
-      roomNumber: 3,
-      roomType: 'Private Suit Room',
-      amenity: 'AC, Free Wifi,tv,bathrom,kitchen',
-      price: 15000,
-      photos:
-        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nytimes.com%2F2019%2F03%2F20%2Ftravel%2Fshop-hotel-rooms-boutiques-retail.html&psig=AOvVaw3buJ-84TWAHhlYuODHhb0G&ust=1681731559259000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNCx7riorv4CFQAAAAAdAAAAABAE',
-      checkinTime: new Date('10-Nov-2021'),
-      checkoutTime: new Date('20-Nov-2021'),
-      rating: 2.5,
-    },
-  ];
+  roomsList: RoomList[] = [];
+
+  ngOnInit(): void {
+    this.roomsList = [
+      {
+        roomNumber: 1,
+        roomType: 'Deluxe Room',
+        amenity: 'AC, Free Wifi,tv,bathrom,kitchen',
+        price: 500,
+        photos:
+          'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nytimes.com%2F2019%2F03%2F20%2Ftravel%2Fshop-hotel-rooms-boutiques-retail.html&psig=AOvVaw3buJ-84TWAHhlYuODHhb0G&ust=1681731559259000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNCx7riorv4CFQAAAAAdAAAAABAE',
+        checkinTime: new Date('11-Nov-2021'),
+        checkoutTime: new Date('21-Nov-2021'),
+        rating: 4.5,
+      },
+      {
+        roomNumber: 2,
+        roomType: 'Deluxe Double Room',
+        amenity: 'AC, Free Wifi,tv,bathrom,kitchen',
+        price: 1000,
+        photos:
+          'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nytimes.com%2F2019%2F03%2F20%2Ftravel%2Fshop-hotel-rooms-boutiques-retail.html&psig=AOvVaw3buJ-84TWAHhlYuODHhb0G&ust=1681731559259000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNCx7riorv4CFQAAAAAdAAAAABAE',
+        checkinTime: new Date('10-Nov-2021'),
+        checkoutTime: new Date('20-Nov-2021'),
+        rating: 3.4,
+      },
+      {
+        roomNumber: 3,
+        roomType: 'Private Suit Room',
+        amenity: 'AC, Free Wifi,tv,bathrom,kitchen',
+        price: 15000,
+        photos:
+          'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nytimes.com%2F2019%2F03%2F20%2Ftravel%2Fshop-hotel-rooms-boutiques-retail.html&psig=AOvVaw3buJ-84TWAHhlYuODHhb0G&ust=1681731559259000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNCx7riorv4CFQAAAAAdAAAAABAE',
+        checkinTime: new Date('10-Nov-2021'),
+        checkoutTime: new Date('20-Nov-2021'),
+        rating: 2.5,
+      },
+    ];
+  }
 
   toggle() {
     this.hideRooms = !this.hideRooms;
+  }
+
+  selectRoom(room: RoomList) {
+    this.selectedRoom = room;
+  }
+
+  addRoom() {
+    const room: RoomList = {
+      roomNumber: 4,
+      roomType: 'Deluxe Room',
+      amenity: 'Ac, free wifi , tv , bathroom',
+      price: 500,
+      photos:
+        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nytimes.com%2F2019%2F03%2F20%2Ftravel%2Fshop-hotel-rooms-boutiques-retail.html&psig=AOvVaw3buJ-84TWAHhlYuODHhb0G&ust=1681731559259000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNCx7riorv4CFQAAAAAdAAAAABAE',
+      checkinTime: new Date('10-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021'),
+      rating: 3.3,
+    };
+    //this.roomsList.push(room);
+    this.roomsList = [...this.roomsList, room];
   }
 }
